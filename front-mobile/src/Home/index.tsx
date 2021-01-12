df344a7d2e17ba7339fa3e6567a0e080120717e9
Gleyson Miranda Souza <gleydson.m.s.1993@gmail.com>
2021-01-12 08:23:43 -0300

    Add order list

--- front-mobile/src/Home/index.tsx
+++ front-mobile/src/Home/index.tsx
@@ -1,15 +1,19 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import Header from '../Header';
 
 export default function Home() {
+  const navigation = useNavigation();
   
   const handleOnPress = () => {
-
+    navigation.navigate('Orders');
   }
 
   return (
     <>
+    <Header />
     <View style={styles.container}>
       <Image source={require('../assets/deliveryman.png')} />
       <Text style={styles.title}>
@@ -28,8 +32,6 @@ export default function Home() {
     </>
   );
 }
-
-{/*55:04*/}
 
 const styles = StyleSheet.create({
     container: {
